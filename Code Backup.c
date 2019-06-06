e1c5243553161fdc163b48e32c3b5508121ea521
Nhan Nguyen <48233282+tinominor@users.noreply.github.com>
2019-06-05 23:12:24 -0700

    Changed over to solar tracker
    Completed the code for solar tracking with the motors

--- Code Backup.c
+++ Code Backup.c
@@ -1,28 +1,23 @@
-#include <FastLED.h>
-#include "MeOrion.h"
-#include <SoftwareSerial.h>
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
-#include <utility/imumaths.h>
+#include "MeOrion.h"            //Library for the Motor driver
+#include <SoftwareSerial.h>     //Library for the Motor driver
+#include <Adafruit_Sensor.h>    //Library for the BNO055
+#include <Adafruit_BNO055.h>    //Library for the BNO055
 #include <math.h>
-
-float wheelmass = 50;   //used for calculation of Inertia
-float wheelradius = 50; //used for calculation of Inertia
-float cubemass = 20;
-float gravity = 9.8;
+#include <Wire.h>
 
   //Gyro Sensor
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
   //Motor Controller
-MeEncoderNew xmotor(0x09, SLOT1); //  Motor at slot1 .  can use the example library in Makeblock to change address
+MeEncoderNew ymotor(0x09, SLOT1); //  Motor at slot1 .  can use the example library in Makeblock to change address
 MeEncoderNew zmotor(0x09, SLOT2); //  motor at slot2
 
-  //LEDs
-#define NUM_LEDS 5    // How many leds are in the strip?
-#define DATA_PIN 5    // Data pin that led data will be written out over
-CRGB leds[NUM_LEDS];  // This is an array of leds.  One item for each led in your strip.
+  //Photohoresistors for sensor. TopLeft = pr1, TR = pr2, BL = pr3, BR = pr4
+int pr1 = A0;
+int pr2 = A1;
+int pr3 = A2;
+int pr4 = A3;
+int tol = A6;   //Potentiometer to adjust the sensitivity to the ambient lighting
 
 void setup(){
   Serial.begin(9600);
@@ -39,90 +34,100 @@ void setup(){
  *  Z acis spins along the global z (outward or inward) axis
  */
 //------------------------------Setup for Motor Controller------------------------------
-  xmotor.begin();
-  zmotor.begin();
-//------------------------------Setup for LEDs------------------------------
-  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+  ymotor.begin();   //motor1
+  zmotor.begin();   //motor2
+//------------------------------Setup for PhotoResistor------------------------------
+  pinMode(pr1, INPUT);
+  pinMode(pr2, INPUT);
+  pinMode(pr3, INPUT);
+  pinMode(pr4, INPUT);
+  pinMode(tol, INPUT);
 }
 
 void loop(){
-  //DON'T REMOVE BELOW LINE. This is where you're able to extract angle value.
-  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-  //This is the tested range of the each axis. Use this to 'map' your values.
-  //x range: 0-360
-  //y range: -90-90
-  //z range: -180-180
-  int val_X = map(euler.y(), 0, 360, -25, 25);  //remap the limits from -10 and 90 to 0 and 5.
-  int val_Y = map(euler.y(), -90, 90, -25, 25);  //remap the limits from -10 and 90 to 0 and 5.
-  int val_Z = map(euler.y(), -180, 180, -25, 25);  //remap the limits from -10 and 90 to 0 and 5.
-  val = val*10;
-  rotateZ(val);
-  rotateX(val);
+  //This is the main function. This recalibrate all the sensors to have the same range.
+  //This is intended to be very intuitive, so that if you want to test anything, you can-
+  //do so without rewriting the whole code.
+  int pr1val = map(analogRead(pr1), 0, 1024, 0, 100); //800 is max value
+  int pr2val = map(analogRead(pr2), 0, 1024, 0, 100); //840 is max value
+  int pr3val = map(analogRead(pr3), 0, 1024, 0, 100); //870 is max value
+  int pr4val = map(analogRead(pr4), 0, 1024, 0, 100); //870 is max value
+  int tolval = map(analogRead(tol), 0, 1024, 0, 100);
+
+  sensorreading(pr1val, pr2val, pr3val, pr4val, tolval);
 }
+
+void rotateY(int i){
+  //This is to spin motor 1.
+  i = i*10;   //Factors of the angular vecloity
+  ymotor.runSpeed(i);
+}
+
 void rotateZ(int i){
-  /*Y axis rotation should be passed through
-   * positive value is tilting back
-   * negative value is tilting forward
-   */
-  if(i < 0)                     //We want to spin CW so that the angular momentum will push it back
-    zmotor.runSpeed(i*(-10));   //assumming that motor can handle 10k rpm. will spin accordingly to the magnitude of it's angle
-  if(i > 0)                     //We want to spin CCW so that the angular momentum will push it forward
-    zmotor.runSpeed(i*10);
-  //Serial.println(i);
+  //This is to spin motor 2.
+  i = i*10;   //Factors of the angular velocity
+  zmotor.runSpeed(i);
 }
 
-void rotateX(int i){
-  /*Z axis rotation should be passed through
-   * positive value is tilting right
-   * negative value is tilting left
-   */
-  if(i < 0)                     //We want to spin CW so that the angular momentum will push it left
-    xmotor.runSpeed(i);   //assumming that motor can handle 10k rpm. will spin accordingly to the magnitude of it's angle
-  if(i > 0)                     //We want to spin CCW so that the angular momentum will push it right
-    xmotor.runSpeed(i);
-    
-  //Serial.print("pos: ");
-  //Serial.println(xmotor.getCurrentPosition());
+int avg(int x, int y){
+  //finding the averages of the 2 values.
+  int total = (x+y)/2;
+  return (total);
 }
-/*int mathangularVelo(float i){
-  //I is theta, of an axis
-    /*    negative i = ccw. correcting motor should spin cw
-    *     positive i = cw. correcting motor should spin ccw
-    *
-  float angmom = cubemass*gravity;//*cos(abs(i));
-  float inertia = wheelmass*wheelradius^2;
-  float velocity = angmom-inertia;
-  if(i > 0)
-    velocity = velocity*(-1);
-  return();
+
+int absdiff(int x, int y){  
+  //Obtainign the absolute value of two values.
+  int diff = x-y;
+  int absolutediff = abs(diff);
+  return absolutediff;
 }
-*/
 
+void sensorreading(int pr1, int pr2, int pr3, int pr4, int tolval){
+  //The core function that takes in the signals and activate the motors accordingly
+  if(absdiff(avg(pr1, pr2), avg(pr3, pr4)) > tolval){  //Check the sensitivity of top and bottom
+    if(avg(pr1, pr2) > avg(pr3, pr4)){  //if top is greater than bottom
+      rotateY(absdiff(avg(pr1, pr2), avg(pr3, pr4)));
+      Serial.println("Up");
+    }
+    else if(avg(pr1, pr2) < avg(pr3, pr4)){   //if bottom is greater than top
+      rotateY(absdiff(avg(pr1, pr2), avg(pr3, pr4))*(-1));
+      Serial.println("Down");
+    }
+  }
 
-
-
-
-
-
-
-
-
-
-
-/*    LEDs TESTING  */
-void blinktest(){
-  for(int i = 0; i < NUM_LEDS; i = i + 1) {
-    // Cycling on and off through all the LEDs
-    leds[i] = CRGB::Red;
-    FastLED.show();   // Make the LED pop up.
-    delay(100);
-    leds[i] = CRGB::Black;   // Turn our current led back to black for the next loop around
+  if(absdiff(avg(pr1, pr3), avg(pr2, pr4)) > tolval){   //Check the sensitivity of left and right
+    if(avg(pr1, pr3) > avg(pr2, pr4)){    //If left is greater than right
+      rotateZ(absdiff(avg(pr1, pr3), avg(pr2, pr4)));
+      Serial.println("Left");
+    }
+    else if(avg(pr1, pr3) < avg(pr2, pr4)){   //If right is greater than left
+      rotateZ(absdiff(avg(pr1, pr3), avg(pr2, pr4))*(-1));
+      Serial.println("Right");
+    }
   }
 }
-void gyrolights(int value){
-  leds[value] = CRGB::Orange; //Set the angle value of the BNO055 to the index of array.
-  FastLED.show();
+
+void printbno0555(int x, int y, int z){
+    /* Display the floating point data */
+    //USED FOR DEBUGGING THE GYRO.
+  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  Serial.print("X: ");
+  Serial.print(x);
+  Serial.print(" Y: ");
+  Serial.print(y);
+  Serial.print(" Z: ");
+  Serial.print(z);
+  Serial.print("\t\t");
+  /* Display calibration status for each sensor. */
+  uint8_t system, gyro, accel, mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
+  Serial.print("CALIBRATION: Sys=");
+  Serial.print(system, DEC);
+  Serial.print(" Gyro=");
+  Serial.print(gyro, DEC);
+  Serial.print(" Accel=");
+  Serial.print(accel, DEC);
+  Serial.print(" Mag=");
+  Serial.println(mag, DEC);
   delay(100);
-  leds[value-1] = CRGB::Black;
-  leds[value+1] = CRGB::Black;
 }
